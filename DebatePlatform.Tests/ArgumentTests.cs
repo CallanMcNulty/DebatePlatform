d6c7e0aef02feae89b3c78bc1b7d0771be2dfdec
Callan McNulty <jabberwocky222@gmail.com>
2016-09-16 10:10:09 -0700

    Add Testing project and add some tests for Argument Model

---
+++ DebatePlatform.Tests/ArgumentTests.cs
@@ -0,0 +1,120 @@
+﻿using DebatePlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DebatePlatform.Tests
+{
+    public class ArgumentTests
+    {
+        [Fact]
+        public void CanParentTest()
+        {
+            //Arrange
+            Argument arg = new Argument();
+            arg.ArgumentId = 1;
+            Argument arg2 = new Argument();
+            arg2.ArgumentId = 2;
+            //Act
+            arg.Parent = arg2;
+            //Assert
+            Assert.Equal(2, arg.Parent.ArgumentId);
+        }
+        [Fact]
+        public void GetTotalStrengthMultipleAdditionsTest()
+        {
+            //Arrange
+            Argument arg1 = new Argument();
+            arg1.ArgumentId = 1;
+            arg1.Strength = 1;
+            arg1.IsAffirmative = true;
+            Argument arg2 = new Argument();
+            arg2.ArgumentId = 2;
+            arg2.Strength = 1;
+            arg2.IsAffirmative = true;
+            Argument arg3 = new Argument();
+            arg3.ArgumentId = 3;
+            arg3.Strength = 1;
+            arg3.IsAffirmative = true;
+            arg2.Parent = arg1;
+            arg1.Children = new List<Argument>() { arg2 };
+            arg3.Parent = arg2;
+            arg2.Children = new List<Argument>() { arg3 };
+            arg3.Children = new List<Argument>();
+            //Act
+            int result = arg1.GetTotalStrength();
+            //Assert
+            Assert.Equal(3, result);
+        }
+        [Fact]
+        public void GetTotalStrengthSubtractionTest()
+        {
+            //Arrange
+            Argument arg1 = new Argument();
+            arg1.ArgumentId = 1;
+            arg1.Strength = 1;
+            arg1.IsAffirmative = true;
+            Argument arg2 = new Argument();
+            arg2.ArgumentId = 2;
+            arg2.Strength = 1;
+            arg2.IsAffirmative = false;
+            arg2.Parent = arg1;
+            arg1.Children = new List<Argument>() { arg2 };
+            arg2.Children = new List<Argument>();
+            //Act
+            int result = arg1.GetTotalStrength();
+            //Assert
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void GetTotalStrengthSubtractionOverflowTest()
+        {
+            //Arrange
+            Argument arg1 = new Argument();
+            arg1.ArgumentId = 1;
+            arg1.Strength = 1;
+            arg1.IsAffirmative = true;
+            arg1.ParentId = 10;
+            Argument arg2 = new Argument();
+            arg2.ArgumentId = 2;
+            arg2.Strength = 5;
+            arg2.IsAffirmative = false;
+            arg2.Parent = arg1;
+            arg1.Children = new List<Argument>() { arg2 };
+            arg2.Children = new List<Argument>();
+            //Act
+            int result = arg1.GetTotalStrength();
+            //Assert
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void GetTotalStrengthSubtractionFromSupportTest()
+        {
+            //Arrange
+            Argument arg1 = new Argument();
+            arg1.ArgumentId = 1;
+            arg1.Strength = 1;
+            arg1.IsAffirmative = true;
+            Argument arg2 = new Argument();
+            arg2.ArgumentId = 2;
+            arg2.Strength = 1;
+            arg2.IsAffirmative = true;
+            Argument arg3 = new Argument();
+            arg3.ArgumentId = 3;
+            arg3.Strength = 1;
+            arg3.IsAffirmative = false;
+            arg2.Parent = arg1;
+            arg1.Children = new List<Argument>() { arg2 };
+            arg3.Parent = arg2;
+            arg2.Children = new List<Argument>() { arg3 };
+            arg3.Children = new List<Argument>();
+            //Act
+            int result = arg1.GetTotalStrength();
+            //Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
+
---
+++ DebatePlatform.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DebatePlatform.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ea0b108-d11a-4a8a-894a-094c9f0f6e6c")]
